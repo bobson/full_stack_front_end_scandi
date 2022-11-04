b63cbaa4317533a9fe07771c8d7e4f51de9734a0
SlobodanMarkoski <bmarkoski@yahoo.com>
2022-11-04 16:43:19 +0100

    Add page not found

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Navigation from "./components/navigation/navigation";
 import CategoryPage from "./routes/category-page/category-page";
 import ProductPage from "./routes/product-page/product-page";
 import CartPage from "./routes/cart-page/cart-page";
+import PageNotFound from "./routes/page-not-found/page-not-found";
 
 class App extends Component {
   render() {
@@ -22,6 +23,7 @@ class App extends Component {
               <Route index element={<CategoryPage />} />
               <Route path="product/:id" element={<ProductPage />} />
               <Route path="cart" element={<CartPage />} />
+              <Route path="*" element={<PageNotFound />} />
             </Route>
           </Routes>
         </CartProvider>
